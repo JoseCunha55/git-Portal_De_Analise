ad0c4943b1c4c37650dd65e99da88eab6428391c
Jose Antonio Cunha <jose.cunha@ifrn.edu.br>
2016-12-19 16:23:50 -0300

    Helper
    Helper comitado em 19_12_2016

---
+++ Helpers/ThemeChangeCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Controls.Theming;
+using System.Windows;
+
+namespace PortalEducase.Helpers
+{
+    public class ThemeChangeCommand : ICommand
+    {
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            Theme themeContainer =
+                  (Theme)((FrameworkElement)Application.Current.RootVisual).FindName("ThemeContainer");
+
+            string themeName = parameter as string;
+
+
+            if (themeName == null)
+            {
+                themeContainer.ThemeUri = null;
+            }
+            else
+            {
+                themeContainer.ThemeUri =
+                     new Uri("/System.Windows.Controls.Theming." + themeName +
+                     ";component/Theme.xaml", UriKind.RelativeOrAbsolute);
+            }
+
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, new EventArgs());
+        }
+
+        #endregion
+    }
+}
