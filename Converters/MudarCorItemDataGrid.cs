5144b46c15990657974dde0f0ec427162eb03506
Jose Antonio Cunha <jose.cunha@ifrn.edu.br>
2016-12-19 16:21:37 -0300

    Converters
    Converters comitado em 19_12_2016

---
+++ Converters/MudarCorItemDataGrid.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Portal_De_Analise.Converters
+{
+    public class MudarCorItemDataGrid: IValueConverter
+    {
+       
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal number;
+            number = (decimal)value;
+            if(number <=35)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else if (number>35 && number<60)
+            {
+                return new SolidColorBrush(Colors.Yellow);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Green); ;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Não preciso atualizar o objeto fonte.
+            return null;
+        }
+
+    }
+}
---
+++ Converters/PercentSucessoEtnia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Portal_De_Analise.Converters
+{
+    public class PercentSucessoEtnia: IValueConverter 
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //format the double as a percentage with two decimal points
+            double suc = (Math.Pow((int)value, 1)) * (Math.Pow(Math.E, -(int)value)) / 1;
+            return String.Format("{0:P2}", suc);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //Não preciso atualizar o objeto fonte.
+            return null;
+        }
+
+    }
+}
